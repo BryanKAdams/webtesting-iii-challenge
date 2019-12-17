1152c4cfb2a57312cba78c02297c5760010f98bd
BryanKAdams <17adamsbryk@washk12.org>
2019-12-17 16:50:12 -0700

    Finished MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react'
+import Controls from './Controls'
+import '@testing-library/jest-dom/extend-expect';
+
+test("Must be unlocked before opening", () => {
+    const locktestOpen = render(<Controls locked={true} closed={true} />)
+    expect(locktestOpen.getByTestId("handle")).not.toBeEnabled();
+})
+
+test("Must be unlocked before closing", () => {
+    const locktestClosed = render(<Controls locked={true} closed={false} />)
+    expect(locktestClosed.getByTestId("handle")).not.toBeEnabled()
+})
